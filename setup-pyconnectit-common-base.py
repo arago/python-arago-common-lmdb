077c5d437ed453fea98691ea9096df4c0b7957b0
mklemm2 <mklemm@arago.de>
2017-04-07 19:41:54 +0200

    Netcool adapter import

---
+++ setup-pyconnectit-common-base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+if os.environ.get('USER','') == 'vagrant':
+    del os.link
+
+import distutils.core
+
+distutils.core.setup(
+	name = "arago-pyconnectit-common-base",
+	version = "2.1",
+	author = "Marcus Klemm",
+	author_email = "mklemm@arago.de",
+	description = ("Common functions and classes for pyconnectit"),
+	license = "MIT",
+	url = "http://www.arago.de",
+	long_description="Common functions and classes for pyconnectit",
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Topic :: Utilities",
+		"License :: OSI Approved :: MIT License",
+	],
+	packages=['arago.pyconnectit.common.delta_store',
+			  'arago.pyconnectit.common.lmdb_queue'
+	]
+)
